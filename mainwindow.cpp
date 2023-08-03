8fce124cb4f3ed0e77e211d65492a9e5a2e91b2a
matzamat <48964518+matzamat@users.noreply.github.com>
2023-08-03 14:24:51 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,64 +2,72 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
-QMainWindow(parent),
-ui(new Ui::MainWindow)
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
-ui->setupUi(this);
+    ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
-delete ui;
+    delete ui;
 }
-
 
 void MainWindow::openFile()
 {
-QString filename = QFileDialog::getOpenFileName(this, "Open the file");
-QFile file(filename);
-currentFile = filename;
+    // Open file dialog to select a file
+    QString filename = QFileDialog::getOpenFileName(this, "Open the file");
+    QFile file(filename);
+    currentFile = filename;
 
-if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
-    QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
-    return;
-}
+    // If the file cannot be opened, show a warning message box and return
+    if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+        return;
+    }
 
-ui->textBrowser->setText(filename);
-setWindowTitle(filename);
+    // Set the text browser widget with the selected file name
+    ui->textBrowser->setText(filename);
+    setWindowTitle(filename);
 
-QTextStream in(&file);
-QString text = in.readAll();
-ui->textEdit->setText(text);
+    // Read the contents of the file using a QTextStream and set the text edit widget with the file content
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
 
-file.close();
+    file.close();
 }
 
 void MainWindow::saveFile()
 {
-QString filename = QFileDialog::getSaveFileName(this, "Save as");
-QFile file(filename);
+    // Open file dialog to specify the save location and file name
+    QString filename = QFileDialog::getSaveFileName(this, "Save as");
+    QFile file(filename);
 
-if (!file.open(QFile::WriteOnly | QFile::Text)) {
-    QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
-    return;
-}
+    // If the file cannot be opened for writing, show a warning message box and return
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
 
-currentFile = filename;
-setWindowTitle(filename);
+    // Set the current file and window title to the selected file name
+    currentFile = filename;
+    setWindowTitle(filename);
 
-QTextStream out(&file);
-QString text = ui->textEdit->toPlainText();
-out << text;
+    // Write the contents of the text edit widget to the file using a QTextStream
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
 
-file.close();
+    file.close();
 }
 
 void MainWindow::on_comboBox_activated(int index)
 {
-if (index == 0) {
-openFile();
-} else if (index == 1) {
-saveFile();
-}
+    // When the combo box selection changes, check the index and perform either openFile() or saveFile()
+    if (index == 0) {
+        openFile();
+    } else if (index == 1) {
+        saveFile();
+    }
 }
